f04006d81611b4bda6965e43bc78869d35bb2979
Kimberly Chuc <kimmchuc@gmail.com>
2021-08-06 16:21:14 -1000

    added set, get, and delete endpoints. each query should be working

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class Data(db.Model):
-    # table containing key, value pairs
+    # table containing key, value as columns
 
-    __tablename__ = 'data'
+    __tablename__ = "data"
 
     data_id = db.Column(db.Integer,
                         primary_key=True,
@@ -22,19 +22,18 @@ class Data(db.Model):
 
     def __repr__(self):
         # human readable representation of key,value pair
-
         return f"""<Data data_id={self.data_id} key={self.key} value={self.value}>"""
 
 def connect_to_db(app):
     # connects db to Flask app
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///correlated"
-    app.config['SQLALCHEMY_ECHO'] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///correlated"
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
-    db.create_all()
     db.init_app(app)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # from server import app
     connect_to_db(app)
+    db.create_all()
--- server.py
+++ server.py
@@ -6,52 +6,125 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     #shows homepage
-    return 'index'
+    return 'index.html'
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    #returns user error and status code for 4XX errors
+    if str(e) >= '400':
+        return make_response(jsonify({
+            'message': 'JSON not received, user error',
+            'status code': str(e)}))
+    #returns server error and status code for 5XX errors
+    elif str(e) >= '500':
+        return make_response(jsonify({
+            'message': 'JSON not received, server error',
+            'status code': str(e)}))
+    #shows status code for all other errors
+    raise e
 
 @app.route('/set', methods=['POST'])
 def set_info():
+    #get key & value from json, check to see if it exists in db
+    #if not add it to db, if it is replace with new value
+    data = request.get_json()
+    incoming_key = data['key']
+    incoming_value = data['value']
 
-    if request.is_json:
-        data = request.get_json()
-
-        datatable = Data.query.filter_by(key="key").first()
-        if not datatable:
-            datatable = Data(key="key", value="value")
-            db.session.add(datatable)
-            db.session.commit()
-            response_body = {
-                'message': 'JSON received, new key and value added to db',
-                'key': data.get("key"),
-                'value': data.get("value")
-            }
-        else:
-            existing_key = Data.query.get("key")
-            data.value = existing_key.value
-            db.session.commit()
-            response_body = {
-                'message': 'JSON received, value has been updated',
-                'key': data.get("key"),
-                'value': data.get("value")
-            }
-
-        res = make_response(jsonify(response_body), 200)
-        return res
+    data_table = Data.query.filter_by(key=incoming_key).first()
     
+    if not data_table:
+        data_table = Data(key=incoming_key, value=incoming_value)
+        db.session.add(data_table)
+        db.session.commit()
+
+        response_body = {
+            'message': 'JSON received, new key and value added to db',
+            'key': data.get("key"),
+            'value': data.get("value"),
+            'status code': '200'
+        }
+        res = make_response(jsonify(response_body), 200)
     else:
-        res = make_response(jsonify({'message': 'JSON not received'}), 400)
-        return res
+        value_updated = Data.query.filter_by(key=incoming_key).update(dict(value=incoming_value))
+        db.session.commit()
+        response_body = {
+            'message': 'JSON received, value has been updated',
+            'key': data.get("key"),
+            'value': data.get("value"),
+            'status code': '200'
+        }
+        res = make_response(jsonify(response_body), 200)
+
+    return res
 
 @app.route('/get', methods=['GET'])
-def get_info():
+def get_key():
+    #get the key value that's included in /get?key=someKey url
+    #if a parameter isn't passed, ask user to include key
+    #if there is a parameter, query for the key in the db
+    #if can't find, return message saying it's not in db
+    search_key = request.args.get('key')
 
-    datatable = Data.query.filter_by("key")
-    json_data = request.json
-    json_key = json_data["key"]
-    json_value = json_data["value"]
+    if not search_key:
+        response_body = {
+            'message': "key not input, please include the key you're searching for in the url parameters",
+        }
+    else:
+        found_object = Data.query.filter_by(key=search_key).first()
+        print(found_object)
+        if found_object:
+            found_key = found_object.key
+            found_value = found_object.value
+            print(found_value)
+            response_body = {
+                'key': found_key,
+                'value': found_value,
+            }
+        else:
+            response_body = {
+                'message': 'key not found in database',
+            }
+    res = make_response(jsonify(response_body), 200)
+    return res
 
-    return 
+@app.route('/delete', methods=['POST'])
+def delete_key():
+    #get key from JSON, 
+    #if no key input return message asking for key
+    #if there is a key, query for the key in the db and delete
+    #if key isn't in db, return message saying its not in db
+
+    #could also make an error handler for 400 error in this specific endpoint
+    incoming_data = request.get_json(silent=True)
+    print(incoming_data)
+
+    if incoming_data is None:
+        response_body = {
+            'message': "key parameter is missing, please include the key you'd like to delete in the JSON POST body"
+        }
+        res = make_response(jsonify(response_body), 200)
+    else:
+        requested_key = incoming_data['key']
+        found_key = Data.query.filter_by(key=requested_key).first()
+        if found_key:
+            Data.query.filter_by(key=requested_key).delete()
+            db.session.commit()
+            response_body = {
+                'message': "the key: " + found_key.key + " has been deleted"
+            }
+        else:
+            response_body = {
+                'message': "key does not exist within database, please input existing key"
+            }
+        res = make_response(jsonify(response_body), 200)
+
+    return res
+
 
 if __name__ == '__main__':
     #run app in debug mode on port 4000
-    app.run(debug=True, port=4000)
     connect_to_db(app)
+    app.run(debug=True, port=4000)
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    app.config['TRAP_HTTP_EXCEPTIONS'] = True
